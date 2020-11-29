7f2efe0e7a2b6568d0b3c74c033c17a1f2b101cd
Corey Smith <CSSmith1997@gmail.com>
2020-11-29 13:45:56 -0700

    Added code to the Intern, Employee, Manager, Engineer scripts inside of the Develop folder.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputPath = path.join(OUTPUT_DIR, "./Develop/templates/team.html");
 
 const render = require("./Develop/lib/htmlRenderer");
 const employees = [];
