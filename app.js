706b3ed56440a755ec4d9f684bedd02b9161a8d6
Corey Smith <CSSmith1997@gmail.com>
2020-11-29 12:35:36 -0700

    Added prompt for users to input information

--- app.js
+++ app.js
@@ -4,3 +4,39 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+const employees = [];
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+          },
+          {
+            type: 'list',
+            message: 'What is your role',
+            name: 'role',
+            choices: [
+              "Engineer",
+              "Intern",
+              "Manager"
+            ],
+          },
+          {
+            type: 'input',
+            message: 'What is your ID number?',
+            name: 'id',
+          },
+          {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+          }
+        ]).then((response) => {   
+    ])
