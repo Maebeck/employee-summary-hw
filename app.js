f251d67efd113f0377e17ad225f81ddc84e9e99a
Corey Smith <CSSmith1997@gmail.com>
2020-11-29 12:48:03 -0700

    Added functionality to pushing employee info to html

--- app.js
+++ app.js
@@ -17,26 +17,95 @@ inquirer
             type: 'input',
             message: 'What is your name?',
             name: 'name',
-          },
-          {
+        },
+        {
             type: 'list',
-            message: 'What is your role',
+            message: 'What is your role?',
             name: 'role',
             choices: [
-              "Engineer",
-              "Intern",
-              "Manager"
+                "Engineer",
+                "Intern",
+                "Manager"
             ],
-          },
-          {
+        },
+        {
             type: 'input',
             message: 'What is your ID number?',
             name: 'id',
-          },
-          {
+        },
+        {
             type: 'input',
             message: 'What is your email?',
             name: 'email',
+        }
+    ]).then((response) => {
+        switch (response.role) {
+          case "Manager":
+            inquirer.prompt({
+              type: 'number',
+              name: 'office',
+              message: "What is your office number?",
+              validate: value => isNaN(parseInt(value)) ? 'Please enter a number!' : true
+            }).then((answer) => {
+              employees.push(new Manager(response.name, response.id, response.email, response.role, answer.office));
+              askAgain()
+            });
+            break;
+    
+          case "Engineer":
+            inquirer.prompt({
+              type: 'input',
+              name: 'github',
+              message: 'What is your Github username?',
+              validate: (github) => {
+                if (github) {
+                  return true
+                } else {
+                  console.log("Enter your username.")
+                  return false
+                }
+              }
+            }).then((answer) => {
+              employees.push(new Engineer(response.name, response.id, response.email, response.role, answer.github));
+              askAgain()
+            });
+            break;
+          case "Intern":
+            inquirer.prompt({
+              type: 'input',
+              name: 'school',
+              message: 'What school did you go to?',
+              validate: (school) => {
+                if (school) {
+                  return true
+                } else {
+                  console.log("Enter your school's name.")
+                  return false
+                }
+              }
+            }).then((answer) => {
+              employees.push(new Intern(response.name, response.id, response.email, response.role, answer.school));
+              askAgain()
+            })
+            break;
+        }
+      })
+
+      function askAgain() {
+        inquirer.prompt([
+          {
+            type: 'confirm',
+            name: 'again',
+            message: "Would you like to enter another employee?"
+      
           }
-        ]).then((response) => {   
-    ])
+        ]).then((response) => {
+          if (response.again === true) {
+            askQuestions();
+          } else {
+            console.log(employees)
+            makeTeam();
+          }
+        })
+      };
+      
